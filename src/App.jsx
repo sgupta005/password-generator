2669de3992f6722a21f0b100eddc47f4a5e11355
Shivam Gupta <shivamgupta02005@gmail.com>
2024-03-19 17:03:49 +0530

    Create basic static layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Heading />
+      <Display />
+      <Generator>
+        <CharacterLength />
+        <Options />
+        <Strength />
+        <GenerateBtn />
+      </Generator>
+    </div>
+  );
 }
 
-export default App
+function Heading() {
+  return <h1>Password Generator</h1>;
+}
+
+function Display() {
+  return <input disabled type="text" placeholder="P4$5W0rD!" />;
+}
+
+// eslint-disable-next-line react/prop-types
+function Generator({ children }) {
+  return <div>{children}</div>;
+}
+
+function CharacterLength() {
+  return (
+    <div className="flex">
+      <div>
+        <p>Character Length</p>
+        <input type="range" min={1} max={20} />
+      </div>
+      <p>10</p>
+    </div>
+  );
+}
+
+function Options() {
+  return (
+    <div>
+      <input type="checkbox" />
+      <label>Include Uppercase Letters</label>
+      <br />
+      <input type="checkbox" />
+      <label>Include Lowercase Letters</label>
+      <br />
+      <input type="checkbox" />
+      <label>Include Numbers</label>
+      <br />
+      <input type="checkbox" />
+      <label>Include Symbols</label>
+      <br />
+    </div>
+  );
+}
+
+function Strength() {
+  return (
+    <div>
+      <p>Strength</p>
+    </div>
+  );
+}
+
+function GenerateBtn() {
+  return <button>Generate</button>;
+}
+
+export default App;
+App;
